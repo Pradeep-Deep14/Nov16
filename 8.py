66aea1c7101e84b2e2f738796a6884ca2bb5599b
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2023-11-16 17:04:30 +0530

    first commit

---
+++ 8.py
@@ -0,0 +1,11 @@
+arr1 = [1, 3, 4, 5, 7]
+arr2 = [2, 3, 5, 6]
+arr3=set(arr1)
+arr4=set(arr2)
+arr5= arr3 .intersection (arr4)
+print(arr5)
+#print(arr3)
+#print(arr2)
+
+#Interview Question
+#Print the common elements of 2 Lists
